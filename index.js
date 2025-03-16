73d7c3791312fbaa3ddf1ab721021ba7ba33e929
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-03-16 21:11:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const server = express();
+const path = require("node:path");
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "ejs");
+
+const PORT = process.env.PORT;
+
+server.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(PORT, () =>
+  console.log(`Express Server running at http://localhost:${PORT}...`)
+);
