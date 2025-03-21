00b7f23c7abfa92aac642554847f9290153045fc
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-03-21 06:39:12 +0100

    Add structure to viewRouter.js

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -6,4 +6,25 @@ viewRouter.get("/", (req, res) => {
   res.render("index");
 });
 
+viewRouter.get("/evidence", (req, res) => {
+  res.render("evidence");
+});
+
+viewRouter.get("/gear", (req, res) => {
+  res.render("gear");
+});
+
+viewRouter.get("/break-time", (req, res) => {
+  res.render("break-time");
+});
+
+viewRouter.get("/miscellaneous", (req, res) => {
+  res.render("miscellaneous");
+});
+
+viewRouter.get("/:category/item/:id", (req, res) => {
+  const { category, id } = req.params;
+  res.render("item");
+});
+
 module.exports = viewRouter;
