7474f2f09cef7d058f4f9fe533e9342f63e42afc
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-03-24 20:53:54 +0100

    Render dynamic category data on the index page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,7 +3,7 @@ const db = require("../db/queries");
 async function getAllCategories(req, res) {
   const categories = await db.getCategories();
   console.log(categories);
-  res.json(categories);
+  res.render("index", { categories });
 }
 
 module.exports = {
